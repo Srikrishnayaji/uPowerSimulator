e752457e7e3cfc2f6c144eb3bfbdaa11c08f14a1
Srikrishnayaji <45878094+Srikrishnayaji@users.noreply.github.com>
2020-04-23 18:42:11 +0530

    Update uPowerSimulator.py

--- uPowerSimulator.py
+++ uPowerSimulator.py
@@ -310,7 +310,7 @@ def execute():
         print_special_regs()
         print("Want the data in a txt file?[Y/n]")
         ans = input()
-        if ans != 'n' || ans != 'N':
+        if ans != 'n' or ans != 'N':
             create_dump_values("Final Data Contents")
 
 if __name__ == '__main__':
